2201dc6b700914e80894511e54e1624caa868b13
guofeng <imeepwni@outlook.com>
2017-06-20 14:19:01 +0800

    complete ArrayPairSum
    Signed-off-by: guofeng <imeepwni@outlook.com>

--- src/main/kotlin/ArrayPairSum.kt
+++ src/main/kotlin/ArrayPairSum.kt
@@ -4,7 +4,25 @@
  */
 
 class ArrayPairSum {
-    fun solution(nums: IntArray): Int {
-        return 0
+    fun solution(numbs: IntArray): Int? {
+
+        val size = numbs.size
+        val n = size / 2
+        if (size !in 1..10000 || size % 2 != 0) return null
+
+        numbs.sort()
+
+        val numbs1 = mutableListOf<Int>()
+        val numbs2 = mutableListOf<Int>()
+
+        for (i in 0..size - 1 step 2) {
+            numbs1.add(numbs[i])
+            numbs2.add(numbs[i + 1])
+        }
+
+        val pairSum: Int = (0..n - 1).sumBy { Math.min(numbs1[it], numbs2[it]) }
+
+        return pairSum
     }
+
 }

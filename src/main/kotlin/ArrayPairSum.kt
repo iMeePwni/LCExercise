43ea63ec5f2c8439ef9ab6bcc1c6f5e0473f5581
guofeng <imeepwni@outlook.com>
2017-06-20 13:51:45 +0800

    init ArrayPairSum
    Signed-off-by: guofeng <imeepwni@outlook.com>

---
+++ src/main/kotlin/ArrayPairSum.kt
@@ -0,0 +1,10 @@
+/**
+ * Created by guofeng on 2017/6/20.
+ * https://leetcode.com/problems/array-partition-i/#/description
+ */
+
+class ArrayPairSum {
+    fun solution(nums: IntArray): Int {
+        return 0
+    }
+}

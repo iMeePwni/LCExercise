b5f7451e0affc8a303bc9ac20b886322c369a8d4
guofeng <imeepwni@outlook.com>
2017-06-20 08:52:01 +0800

    add problem url; change fun name from mergeTwoTrees to solution.
    Signed-off-by: guofeng <imeepwni@outlook.com>

--- src/main/kotlin/MergeTwoTrees.kt
+++ src/main/kotlin/MergeTwoTrees.kt
@@ -1,14 +1,15 @@
 /**
  * Created by guofeng on 2017/6/19.
+ * https://leetcode.com/problems/merge-two-binary-trees/#/description
  */
 class MergeTwoTrees {
 
-    fun mergeTrees(t1: TreeNode?, t2: TreeNode?): TreeNode? {
+    fun solution(t1: TreeNode?, t2: TreeNode?): TreeNode? {
         if (t1 == null && t2 == null) return null
 
         return TreeNode((t1?.middle ?: 0) + (t2?.middle ?: 0),
-                mergeTrees(t1?.left ?: null, t2?.left ?: null),
-                mergeTrees(t1?.right ?: null, t2?.right ?: null))
+                solution(t1?.left ?: null, t2?.left ?: null),
+                solution(t1?.right ?: null, t2?.right ?: null))
     }
 
 

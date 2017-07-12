e9f48d1d493cf5c6dd13fd2dc283fe740fb4413e
guofeng <imeepwni@outlook.com>
2017-07-12 16:01:29 +0800

    优化代码。 逻辑清晰。
    Signed-off-by: guofeng <imeepwni@outlook.com>

--- src/main/kotlin/DiameterOfBinaryTree.kt
+++ src/main/kotlin/DiameterOfBinaryTree.kt
@@ -2,18 +2,19 @@
  * Created by guofeng on 2017/7/12.
  */
 class DiameterOfBinaryTree {
-    fun solution(root: TreeNode): Int {
-        return Math.max((root.left?.sum() ?: 0) + (root.right?.sum() ?: 0), root.sum())
+
+    fun single(treeNode: TreeNode?): Int {
+        if (treeNode == null)
+            return 0
+        return Math.max(single(treeNode.left), single(treeNode.right)) + 1
     }
 
-    fun TreeNode.single(): Int {
-        return Math.max(this.left?.single() ?: 0, this.right?.single() ?: 0) + 1
-    }
-
-    fun TreeNode.sum(): Int {
-        val lr = (this.left?.single() ?: 0) + (this.right?.single() ?: 0)
-        val ls = this.left?.sum() ?: 0
-        val rs = this.right?.sum() ?: 0
+    fun solution(treeNode: TreeNode?): Int {
+        if (treeNode == null)
+            return 0
+        val lr = single(treeNode.left) + single(treeNode.right)
+        val ls = solution(treeNode.left)
+        val rs = solution(treeNode.right)
         return Math.max(Math.max(ls, rs), lr)
     }
 }

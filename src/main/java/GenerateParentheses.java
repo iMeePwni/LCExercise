8df7d522c2904987a81bb921fba13d2dc29dbe37
guofeng <imeepwni@outlook.com>
2017-07-24 16:03:47 +0800

    让自己的思路变得清晰
    Signed-off-by: guofeng <imeepwni@outlook.com>

--- src/main/java/GenerateParentheses.java
+++ src/main/java/GenerateParentheses.java
@@ -10,7 +10,17 @@ public class GenerateParentheses {
         return list;
     }
 
-    private void backtrack(List<String> list, String str, int open, int close, int n) {
-//        if ()
+    private void backtrack(List<String> list, String str, int open, int close, int max) {
+        if (str.length() == max * 2) {
+            list.add(str);
+            return;
+        }
+
+        if (open < max) {
+            backtrack(list, str + "(", open + 1, close, max);
+        }
+        if (close < open) {
+            backtrack(list, str + ")", open, close + 1, max);
+        }
     }
 }
